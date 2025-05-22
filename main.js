38dec748cfada1372f3911a219d7bd1014b03e75
vvnec <122540164+vvnec@users.noreply.github.com>
2025-05-22 16:36:03 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log(2+2)
+console.log('12');
+console.log('Добро'+' пожаловать'+' на курс'+' Виктор');
+// alert('22 мая 2025');
+
+
+let num = 33.5;
+let bigInt = 566566455555555666646555555546464633n;
+let str = 'name';
+let boolean = true;
+let nul = null;
+let undf
+let obj
+
+
+
+
+console.log(num);
+console.log(bigInt);
+console.log(str);
+console.log(boolean);
+console.log(nul);
+console.log(undf);
+console.log(obj);
+
+
+
+
+
+
